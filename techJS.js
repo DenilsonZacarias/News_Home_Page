02f53413f1c9b01c0664f5a585e4608b5a468dde
Denilson Zacarias <dejoarza@gmail.com>
2025-08-04 18:17:02 +0200

    No sidebar, agora temos o botao X que sera usado para fechar o sidebar

--- techJS.js
+++ techJS.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector(".mobileSubMenuImage");
 const sideMenu = document.getElementById("sideMenu");
 const overlay = document.getElementById("overlay");
+const closeBtn = document.querySelector(".closeMenuBtn img");
 
 menuBtn.addEventListener("click", () => {
   sideMenu.classList.add("active");
@@ -9,6 +10,11 @@ menuBtn.addEventListener("click", () => {
 
 // Clicar fora fecha o menu lateral
 overlay.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+closeBtn.addEventListener("click", () => {
   sideMenu.classList.remove("active");
   overlay.classList.remove("active");
 });
